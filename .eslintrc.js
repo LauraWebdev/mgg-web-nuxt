63cde0c050662728d481900b83462f5459d8f431
Laura Heimann <hello@laura.media>
2021-06-21 23:51:26 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,16 @@ module.exports = {
     ],
     // add your custom rules here
     rules: {
-        'vue/html-indent': ['error', 'tab'],
+        "vue/html-indent": "off",
+        "vue/max-attributes-per-line": "off",
+        "vue/html-self-closing": "off",
+        "vue/attributes-order": "off",
+        "vue/singleline-html-element-content-newline": "off",
+        "vue/no-v-html": "off",
+        "indent": "off",
+        "semi": [1, "always"],
+        "no-extra-semi": "off",
+        "object-shorthand": "off",
+        "comma-dangle": "off"
     }
 }
